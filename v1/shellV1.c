2726b69defeddcbffd86238aa1ed7afe1f8e0491
Erik <erik.shoiichi@gmail.com>
2021-06-06 20:31:06 -0300

    version 1 commit

--- v1/shellV1.c
+++ v1/shellV1.c
@@ -7,99 +7,97 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "commands.c"
-#include "parser.c"
+//#include "parser.c"
 
 void clearBuffer(char **buffer, int buffSize);
 void checkMallocOrRealloc(char **buffer);
 void executeCommand(commandLine *_command);
 
 int main(int argc, char **argv){
-        if(strcmp(argv[0], "exit") == 0){
-            return 0;
-        } 
+    if(strcmp(argv[0], "exit") == 0){
+        return 0;
+    } 
 
-        if(argc == 1 && strcmp(argv[0], "ls") != 0){             
-            printf("Correct use: %s <command> <arg1> <arg2> ... <argn>\n", argv[0]);
+    if(argc == 1 && strcmp(argv[0], "ls") != 0){             
+        printf("Correct use: %s <command> <arg1> <arg2> ... <argn>\n", argv[0]);
+        return 0;
+    }
+
+    int maxBuffSize = 10;
+    char **buffer = (char **)malloc(maxBuffSize*sizeof(char**));
+    checkMallocOrRealloc(buffer);
+
+    int currBuffSize = 1;
+    int j = 0;
+
+    commandLine *cmd;
+    cmd = (commandLine *)malloc(sizeof(commandLine*));
+    initCommandLine(cmd);
+            
+    for(int i = 1; i < argc; i++){
+
+        if(strcmp(argv[i],"|")!=0 && i!=argc-1 && strcmp(argv[i],";")!=0 && strcmp(argv[i],"||")!=0 && strcmp(argv[i], "&&")!=0){        
+            if(currBuffSize == maxBuffSize){
+                buffer = realloc(buffer, currBuffSize + maxBuffSize);
+                checkMallocOrRealloc(buffer);
+                maxBuffSize = currBuffSize + maxBuffSize;
+            }
+
+            buffer[j] = argv[i];
+            currBuffSize++;
+            j++;
         }
 
-        else{
-            int maxBuffSize = 10;
-            char **buffer = (char **)malloc(maxBuffSize*sizeof(char**));
-            checkMallocOrRealloc(buffer);
+        else if(strcmp(argv[i], "|") == 0){    //encontrou pipe                                                  
+            // for(int k=0; k<currBuffSize-1; k++){
+            //     printf("buffer[%d] = %s\n", k, buffer[k]);
+            // }
+            insertSimpleCommand(cmd, currBuffSize-1, buffer);
+            clearBuffer(buffer, maxBuffSize);
+            currBuffSize = 1;
+            j = 0;
+        }      
 
-            int currBuffSize = 1;
-            int j = 0;
-
+        else if(strcmp(argv[i], ";") == 0){     //encontrou ";" - comandos independentes
+            insertSimpleCommand(cmd, currBuffSize-1, buffer);
+            executeCommand(cmd);
+            free(cmd);
+                    
             commandLine *cmd;
             cmd = (commandLine *)malloc(sizeof(commandLine*));
             initCommandLine(cmd);
-            
-            for(int i = 0; i < argc; i++){
-
-                if(strcmp(argv[i],"|")!=0 && i!=argc-1 && strcmp(argv[i],";")!=0 && strcmp(argv[i],"||")!=0 && strcmp(argv[i], "&&")!=0){
                     
-                    if(currBuffSize == maxBuffSize){
-                        buffer = realloc(buffer, currBuffSize + maxBuffSize);
-                        checkMallocOrRealloc(buffer);
-                        maxBuffSize = currBuffSize + maxBuffSize;
-                    }
-
-                    buffer[j] = argv[i];
-                    currBuffSize++;
-                    j++;
-                }
-
-                else if(strcmp(argv[i], "|") == 0){    //encontrou pipe                                                  
-                    // for(int k=0; k<currBuffSize-1; k++){
-                    //     printf("buffer[%d] = %s\n", k, buffer[k]);
-                    // }
-                    insertSimpleCommand(cmd, currBuffSize-1, buffer);
-                    clearBuffer(buffer, maxBuffSize);
-                    currBuffSize = 1;
-                    j = 0;
-                }      
-
-                else if(strcmp(argv[i], ";") == 0){     //encontrou ";" - comandos independentes
-                    insertSimpleCommand(cmd, currBuffSize-1, buffer);
-                    executeCommand(cmd);
-                    free(cmd);
-                    
-                    commandLine *cmd;
-                    cmd = (commandLine *)malloc(sizeof(commandLine*));
-                    initCommandLine(cmd);
-                    
-                    clearBuffer(buffer, maxBuffSize);
-                    currBuffSize = 1;
-                    j = 0;
-                }
-
-                else if(strcmp(argv[i], "||") == 0){        //encontrou "||" = OR
-
-                }   
-
-                else if(strcmp(argv[i], "&&") == 0){        //encontrou "&&" = AND
-
-                }
-
-                else if(i == argc - 1){  //fim da linha
-                    if(currBuffSize == maxBuffSize){
-                        buffer = realloc(buffer, currBuffSize + maxBuffSize);
-                        checkMallocOrRealloc(buffer);
-                        maxBuffSize = currBuffSize + maxBuffSize;
-                    }
-
-                    buffer[j] = argv[i];
-                    currBuffSize++;
-                    // for(int k=0; k<currBuffSize-1; k++){
-                    //     printf("buffer[%d] = %s\n", k, buffer[k]);
-                    // }
-                    insertSimpleCommand(cmd, currBuffSize-1, buffer);
-                }   
-            }
-            executeCommand(cmd);
-            free(buffer);
-            free(cmd);
+            clearBuffer(buffer, maxBuffSize);
+            currBuffSize = 1;
+            j = 0;
         }
+
+        else if(strcmp(argv[i], "||") == 0){        //encontrou "||" = OR
+
+        }   
+
+        else if(strcmp(argv[i], "&&") == 0){        //encontrou "&&" = AND
+
+        }
+
+        else if(i == argc - 1){  //fim da linha
+            if(currBuffSize == maxBuffSize){
+                buffer = realloc(buffer, currBuffSize + maxBuffSize);
+                checkMallocOrRealloc(buffer);
+                maxBuffSize = currBuffSize + maxBuffSize;
+            }
+
+            buffer[j] = argv[i];
+            currBuffSize++;
+            // for(int k=0; k<currBuffSize-1; k++){
+            //     printf("buffer[%d] = %s\n", k, buffer[k]);
+            // }
+            insertSimpleCommand(cmd, currBuffSize-1, buffer);
+        }   
+    }
+    executeCommand(cmd);
+    free(buffer);
+    free(cmd);
 }
 
 void clearBuffer(char **buffer, int buffSize){
